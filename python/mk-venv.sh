daa6a67a1d9a2822bfc404588e9245fd8572f52e
ukhanspam@gmail.com <ukhanspam@gmail.com>
2023-05-31 16:28:35 -0400

    modified virtual env script

--- python/mk-venv.sh
+++ python/mk-venv.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-DIR="/Users/ukhan/Development/python-public.git/.venv"
-VENV_NAME="github-public"
+VENV_DIR="/Users/${user}/Development/${GIT_DIR}/.venv"
+VENV_NAME="python-venv"
+VENV="${VENV_DIR}/${VENV_NAME}"
+PYTHON="python3.9"
 
-mkdir -p ${DIR}
-cd ${DIR}
-python3.9 -m venv ${VENV_NAME}
+if [ ! -e "${VENV}" ]; then
+  echo -e "\n Creating new python virtual env: ${VENV}"
+  mkdir -p ${VENV_DIR}
+  cd ${VENV_DIR}
+  ${PYTHON} -m venv ${VENV_NAME}
+else
+  echo -e "\n  Activating python virtual env: ${VENV}"
+fi 
+

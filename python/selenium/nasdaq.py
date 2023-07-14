fe2c1c832f06860d97f352d766cc3993264faf89
Ubaidul Khan <ubaidul.khan@gmail.com>
2023-07-13 21:45:51 -0400

    added selenium scraper

---
+++ python/selenium/nasdaq.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+
+import os
+
+# Add the directory containing the Firefox binary to the PATH environment variable
+os.environ['PATH'] += ':/Applications/Firefox-91.app/Contents/MacOS'
+
+# url = f"https://api.nasdaq.com/api/quote/DKNG/info?assetclass=stocks"
+url = 'https://api.nasdaq.com/api/quote/DKNG/info?assetclass=stocks'
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Referer": "https://www.nasdaq.com/market-activity/stocks/dkng"
+}
+
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox()
+
+
+# Specify the path to the Firefox binary
+driver_path = './geckodriver'
+firefox_binary_path = '/Applications/Firefox-91.app/Contents/MacOS/firefox'
+
+# Create the WebDriver instance with the specified binary location
+# driver = webdriver.Firefox(executable_path='./geckodriver', firefox_binary=firefox_binary)
+# driver = webdriver.Firefox(executable_path=driver_path, firefox_binary=firefox_binary_path)
+driver = webdriver.Firefox()
+
+
+try:
+    # Open the web page
+    # driver.get("https://www.example.com")
+    driver.get(url)
+    
+
+    # Find the table element using XPath
+    table = driver.find_element("xpath", "//table[@class='summary-data__table']")
+
+    # Find all the table data (td) elements within the table using XPath
+    table_data = table.find_elements_by_xpath("//tr/td[normalize-space(text())='Sector']/../.")
+
+    # Loop through the table data elements and print their text content
+    for data in table_data:
+        print(data.text)
+
+except Exception as e:
+    print("Error:", str(e))
+
+finally:
+    # Close the browser window
+    driver.quit()
--- python/ticker-2-csv/csv-maker-3.py
+++ python/ticker-2-csv/csv-maker-3.py
@@ -1,14 +1,28 @@
 import requests
 from lxml import html
+from bs4 import BeautifulSoup
+
 
 url = "https://www.nasdaq.com/market-activity/stocks/dkng"
-headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36'}
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Referer": "https://www.nasdaq.com/market-activity/stocks/dkng"
+}
+
 response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, "html.parser")
+print(soup)
 
+'''
 tree = html.fromstring(response.content)
+print(tree)
 
-sector_xpath = '//span[@class="label__container__3LIA4"]//a'
-sector_element = tree.xpath(sector_xpath)[0]
-sector = sector_element.text.strip()
+# sector_xpath = '//span[@class="label__container__3LIA4"]//a'
+sector_xpath = '//tr/td[normalize-space(text())="Sector"]/../.'
+sector_element = tree.xpath(sector_xpath)
+# sector = sector_element.text.strip()
+sector = sector_element
 
 print("Sector:", sector)
+'''
